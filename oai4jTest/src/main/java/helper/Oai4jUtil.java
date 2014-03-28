eee2e48bfe52776011d284a40888ac1a549d10a0
Simo Simov <simo.simov@ontotext.com>
2014-03-28 11:27:06 +0200

    wrapper for Record fields access.

---
+++ oai4jTest/src/main/java/helper/Oai4jUtil.java
@@ -0,0 +1,39 @@
+package helper;
+
+import org.dom4j.Element;
+import se.kb.oai.pmh.Header;
+import se.kb.oai.pmh.Record;
+import se.kb.xml.XMLUtils;
+
+import java.io.IOException;
+
+/**
+ * Created by Simo on 14-3-7.
+ */
+public class Oai4jUtil {
+    public static String getMetadata(Record record) {
+        Element metadataElement = record.getMetadata();
+
+        String metadata = null;
+        if (metadataElement != null) {
+            try {
+                metadata = XMLUtils.xmlToString(metadataElement);
+            } catch (IOException e) {
+                // shouldn't be possible to reach here.
+                e.printStackTrace();
+            }
+        }
+
+        return metadata;
+    }
+
+    public static String getId(Record record) {
+        String id = null;
+        Header header = record.getHeader();
+        if (header != null) {
+            id = header.getIdentifier();
+        }
+
+        return id;
+    }
+}
