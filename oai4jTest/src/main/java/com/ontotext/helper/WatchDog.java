2ade48fedaac0771cc4c7073a413a888d90a5eff
Simo Simov <simo.simov@ontotext.com>
2014-09-03 14:55:04 +0300

    Changed logging levels; commented out test code; other minor changes

--- oai4jTest/src/main/java/com/ontotext/helper/WatchDog.java
+++ oai4jTest/src/main/java/com/ontotext/helper/WatchDog.java
@@ -3,7 +3,6 @@ package com.ontotext.helper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -14,48 +13,55 @@ public class WatchDog {
     private static Log log = LogFactory.getLog(WatchDog.class);
     private final int timeoutPeriods;
     private Timer timer = new Timer("WatchDog", true);
-    private int intervalsLeft;
+    private int periodsLeft;
 
     public WatchDog(int timeoutMinutes) {
         this(timeoutMinutes, 60*1000);
     }
 
-    public WatchDog(int timeoutPeriods, long periodLength) {
+    public WatchDog(final int timeoutPeriods, long periodLength) {
         this.timeoutPeriods = timeoutPeriods;
-        this.intervalsLeft = timeoutPeriods;
+        this.periodsLeft = timeoutPeriods;
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 synchronized (this) {
-                    if (--intervalsLeft == 0) {
-                        log.info("WD Exit");
+                    if (--periodsLeft == 0) {
+                        log.fatal("Main thread hanged. System.exit(1)");
                         System.exit(1);
-
                     }
-                    log.info("WD Continue " + intervalsLeft);
+                    final String msg = "Hits left " + periodsLeft;
+                    if (periodsLeft *2 < timeoutPeriods) {
+                        log.warn(msg);
+                    } else {
+                        log.debug(msg);
+                    }
                 }
             }
         }, periodLength,  periodLength);
     }
 
     synchronized public void reset() {
-        intervalsLeft = timeoutPeriods;
+        periodsLeft = timeoutPeriods;
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        final int numIntervals = 5;
-        final int intervalLength = 1000;
-
-        final int interval = numIntervals * intervalLength;
-        final int offset = 500;
-        Random random = new Random();
-        WatchDog watchDog = new WatchDog(numIntervals, intervalLength);
-        while (true) {
-            int sleepTime = random.nextInt(interval + offset);
-            log.info("Sleep(" + sleepTime + ")");
-            Thread.sleep(sleepTime);
-            watchDog.reset();
-            log.info("Reset.");
-        }
-    }
+//    public static void main(String[] args) throws InterruptedException {
+//        final int numIntervals = 5;
+//        final int intervalLength = 1000;
+//        final int numIterations = 5;
+//
+//        final int interval = numIntervals * intervalLength;
+//        final int offset = 500;
+//        Random random = new Random();
+//        WatchDog watchDog = new WatchDog(numIntervals, intervalLength);
+//        for (int i = 0; i != numIterations; ++i) {
+//            int sleepTime = random.nextInt(interval + offset);
+//            log.info("Sleep(" + sleepTime + ")");
+//            Thread.sleep(sleepTime);
+//            watchDog.reset();
+//            log.info("Reset.");
+//        }
+//
+//        log.info("Normal exit");
+//    }
 }
